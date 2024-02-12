67ed1daf25415e20077117aab0f6a16427d70419
Thomas Volk (kubuntu) <info@thomasvolk.de>
2024-02-12 09:35:54 +0100

    build framework

---
+++ Makefile
@@ -0,0 +1,35 @@
+all: test
+
+PYTHON=./venv/bin/python3
+FRETTY_BOOK=./venv/bin/fretty-book
+TWINE=./venv/bin/twine
+
+
+venv:
+	python3 -m venv venv
+	$(PYTHON) -m pip install fretty[PNG]
+	$(PYTHON) -m pip install build
+	$(PYTHON) -m pip install twine
+	
+
+$(FRETTY_BOOK): venv
+	$(PYTHON) -m pip install -e .
+
+test: $(FRETTY_BOOK)
+	mkdir -p out
+	@echo "check help ..."
+	$(FRETTY_BOOK) -v
+	$(FRETTY_BOOK) -h
+
+build: test
+	$(PYTHON) -m build --wheel
+
+upload: build
+	PYTHONIOENCODING=utf-8 $(TWINE) upload --repository pypi dist/*
+
+upload-test: build
+	PYTHONIOENCODING=utf-8 $(TWINE) upload --repository testpypi dist/*
+
+clean:
+	rm -rf out build dist *.egg-info venv
+
---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import unittest
+import re
+from os.path import join, dirname, abspath
+
+
+SETUP_DIR = dirname(abspath(__file__))
+PROJECT_URL = 'https://github.com/thomasvolk/fretty-book'
+
+with open(join(SETUP_DIR, 'fretty_book', '__init__.py')) as f:
+    VERSION = re.search("__version__\s=\s'(.*)'", f.read()).group(1)
+
+
+def project_test_suite():
+    test_loader = unittest.TestLoader()
+    test_suite = test_loader.discover('tests', pattern='*_test.py')
+    return test_suite
+
+
+with open('README.md') as f:
+    long_description = f.read()
+
+
+setup(
+    name='fretty-book',
+    description='fretty-book can replace fretty markup with images',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version=VERSION,
+    include_package_data=True,
+    author="Thomas Volk",
+    author_email="info@thomasvolk.de",
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Markup"
+    ],
+    packages=find_packages(exclude=("tests",)),
+    entry_points = {
+        'console_scripts': ['fretty-book=fretty_book:main'],
+    },
+    install_requires=[
+        "fretty[PNG]"
+    ],
+    url=PROJECT_URL,
+    license='Apache',
+    platforms='any',
+    python_requires='>=3.9.0'
+)
